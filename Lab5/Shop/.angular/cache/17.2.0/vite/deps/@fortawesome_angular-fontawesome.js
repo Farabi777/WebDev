be3f22553a5f5de6743f373efa4d7eddb32f39af
Farabi777 <f_kuanysh@kbtu.kz>
2024-02-27 15:53:46 +0600

    SHOP

---
+++ Lab5/Shop/.angular/cache/17.2.0/vite/deps/@fortawesome_angular-fontawesome.js
@@ -0,0 +1,28 @@
+import {
+  FaConfig,
+  FaDuotoneIconComponent,
+  FaIconComponent,
+  FaIconLibrary,
+  FaLayersComponent,
+  FaLayersCounterComponent,
+  FaLayersTextComponent,
+  FaStackComponent,
+  FaStackItemSizeDirective,
+  FontAwesomeModule
+} from "./chunk-GDM3WIT2.js";
+import "./chunk-237HRUMH.js";
+import "./chunk-TPNJYKY4.js";
+import "./chunk-MEBZYEDQ.js";
+export {
+  FaConfig,
+  FaDuotoneIconComponent,
+  FaIconComponent,
+  FaIconLibrary,
+  FaLayersComponent,
+  FaLayersCounterComponent,
+  FaLayersTextComponent,
+  FaStackComponent,
+  FaStackItemSizeDirective,
+  FontAwesomeModule
+};
+//# sourceMappingURL=@fortawesome_angular-fontawesome.js.map
